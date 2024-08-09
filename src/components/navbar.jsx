a654231503c1e50e5e3aaf68f7957bbd5dc1ab6a
Assignments <sanaullah.siddiqui0317'git config --global user.email>
2024-08-10 00:53:24 +0500

    navbar done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './Navbar.css'; // Import custom CSS
 
-const Navbar = () => {
+const NavigationBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Navbar expand="lg" className="custom-navbar">
+      <Container fluid className="p-0 m-0">
+        <Navbar.Brand href="#" className='logo'>Hotel</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto" >
+            <Nav.Link href="#home" className='color'>Home</Nav.Link>
+            <Nav.Link href="#about" className='color'>About</Nav.Link>
+            <Nav.Link href="#services" className='color'>Services</Nav.Link>
+            <Nav.Link href="#rooms" className='color'>Rooms</Nav.Link>
+            <NavDropdown title="Pages" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#email">Booking</NavDropdown.Item>
+              <NavDropdown.Item href="#phone">Our Team</NavDropdown.Item>
+              <NavDropdown.Item href="#address">Testimonial</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#rooms" className='color'>Rooms</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navbar
+export default NavigationBar;
